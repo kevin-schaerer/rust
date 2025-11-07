37176b515df73816c1bd5cf1bbfaafaab8e4f7e8
Kevin Schaerer <kevin.schaerer@nettimelogic.com>
2025-11-07 16:20:03 +0100

    add riscv32imac_unknown_linux_gnu target

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -38,6 +38,7 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
     "aarch64_be-unknown-hermit",
     "aarch64_be-unknown-none-softfloat",
+    "riscv32imac-unknown-linux-gnu",
 ];
 
 /// Minimum version threshold for libstdc++ required when using prebuilt LLVM
